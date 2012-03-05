91a2f00992fefa0378d4c9753bf36bc1008da09e
Robert Chipperfield <robert@syxis.co.uk>
2012-03-05 22:12:06 +0000

    Tidying up from pull request

--- Engine/AdifHandler.cs
+++ Engine/AdifHandler.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
@@ -7,11 +7,11 @@ namespace Engine
 {
     public static class AdifHandler
     {
-        const string zxa_callUsed = "G6UW";    	    // Callsign sent
-        const string zxa_locatorSent = "JO02af";	    // Locator sent
-        const string zxa_contest = "RSGB-UKAC";	    // Contest name
-        const string zxa_section = "AO";              // Contest section
-        const string zxa_operators = "G3ZAY M0ZXA";	// Operators
+        const string c_CallUsed = "G6UW";    	    // Callsign sent
+        const string c_LocatorSent = "JO02af";	    // Locator sent
+        const string c_Contest = "RSGB-UKAC";	    // Contest name
+        const string c_Section = "AO";              // Contest section
+        const string c_Operators = "G3ZAY M0ZXA";	// Operators
         
         public static void ExportContacts(IEnumerable<Contact> contacts, string exportLocation)
         {
@@ -20,16 +20,16 @@ namespace Engine
                 writer.WriteLine("ADIF export from M0VFC CamLog");
                 writer.WriteLine("");
                 writer.WriteLine("---");
-                writer.WriteLine("Callsign : " + zxa_callUsed.ToUpper() + "      SECTION/ZONE: " + zxa_section.ToUpper() + "      Grid square : " + zxa_locatorSent.ToUpper() );
-                writer.WriteLine("Operator(s) : " + zxa_operators.ToUpper());
+                writer.WriteLine("Callsign : " + c_CallUsed.ToUpper() + "      SECTION/ZONE: " + c_Section.ToUpper() + "      Grid square : " + c_LocatorSent.ToUpper() );
+                writer.WriteLine("Operator(s) : " + c_Operators.ToUpper());
                 writer.WriteLine("---");
-                writer.WriteLine("");
-                if (string.IsNullOrEmpty(zxa_contest))
+                writer.WriteLine();
+                if (string.IsNullOrEmpty(c_Contest))
                 {
-                    writer.WriteLine("<CONTEST_ID:" + zxa_contest.Length + ">" + zxa_contest.ToUpper());
+                    writer.WriteLine("<CONTEST_ID:" + c_Contest.Length + ">" + c_Contest.ToUpper());
                 }
                 writer.WriteLine("<EOH>");
-                writer.WriteLine("");
+                writer.WriteLine();
                 
                 foreach (Contact c in contacts)
                     ExportContact(c, writer);
--- Engine/CabrilloExporter.cs
+++ Engine/CabrilloExporter.cs
@@ -7,11 +7,11 @@ namespace Engine
 {
     public static class CabrilloExporter
     {
-        const string zxa_callUsed = "G6UW"; // Callsign sent
-        const string zxa_locatorSent = "JO02af"; // Locator sent
-        const string zxa_contest = "RSGB-UKAC"; // Contest name (optional)
-        const string zxa_operators = "G3ZAY M0ZXA"; // Operators
-        const string zxa_claimedScore = ""; // Claimed score (optional)
+        const string c_CallUsed = "G6UW"; // Callsign sent
+        const string c_LocatorSent = "JO02af"; // Locator sent
+        const string c_Contest = "RSGB-UKAC"; // Contest name (optional)
+        const string c_Operators = "G3ZAY M0ZXA"; // Operators
+        const string c_ClaimedScore = ""; // Claimed score (optional)
 
         public static void ExportContacts(IEnumerable<Contact> contacts, string exportLocation)
         {
@@ -19,17 +19,17 @@ namespace Engine
             {
                 writer.WriteLine("START-OF-LOG: 2.0");
                 writer.WriteLine("CREATED-BY: M0VFC CamLog");
-                if (string.IsNullOrEmpty(zxa_contest))
+                if (string.IsNullOrEmpty(c_Contest))
                 {
-                    writer.WriteLine("CONTEST: " + zxa_contest.ToUpper());
+                    writer.WriteLine("CONTEST: " + c_Contest.ToUpper());
                 }
-                writer.WriteLine("CALLSIGN: " + zxa_callUsed.ToUpper());
-                if (string.IsNullOrEmpty(zxa_claimedScore))
+                writer.WriteLine("CALLSIGN: " + c_CallUsed.ToUpper());
+                if (string.IsNullOrEmpty(c_ClaimedScore))
                 {
-                    writer.WriteLine("CLAIMED SCORE: " + zxa_claimedScore);
+                    writer.WriteLine("CLAIMED SCORE: " + c_ClaimedScore);
                 }
-                writer.WriteLine("OPERATORS: " + zxa_operators.ToUpper());
-                writer.WriteLine("");
+                writer.WriteLine("OPERATORS: " + c_Operators.ToUpper());
+                writer.WriteLine();
                 foreach (Contact c in contacts)
                     ExportContact(c, writer);
             }
@@ -37,49 +37,18 @@ namespace Engine
 
         private static void ExportContact(Contact c, StreamWriter writer)
         {
-           writer.WriteLine("QSO: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}",
-                zxa_ensureLength(BandHelper.ToMHzString(c.Band), 4, ' ', true),
-                ModeHelper.ToCabrilloString(c.Mode),
-                c.StartTime.ToString("yyyy-MM-dd HHmm"),
-                zxa_ensureLength(zxa_callUsed, 15, ' ', false).ToUpper(),
-                zxa_locatorSent.ToUpper(),
-                zxa_ensureLength(c.ReportSent, 3, ' ', false),
-                zxa_ensureLength(c.SerialSent.ToString(), 3, '0', true),
-                zxa_ensureLength(c.Callsign, 15, ' ', false).ToUpper(),
-                c.LocatorReceived.ToString().ToUpper(),
-                zxa_ensureLength(c.ReportReceived, 3, ' ', false),
-                zxa_ensureLength(c.SerialReceived.ToString(), 3, '0', true));
-        }
-
-        ///<summary>
-        /// This allows a minimum length to be
-        /// defined for the export fields, to
-        /// improve readability for Humans.
-        ///
-        /// zxa_string is string to adjust
-        /// zxa_length is the minimum length
-        /// zxa_filler is the character to add to lengthen the string
-        /// zxa_front selects fill at front (1) or back (0) of string
-        ///
-        ///</summary>
-        private static string zxa_ensureLength(string zxa_string, int zxa_length, char zxa_filler, bool zxa_front)
-        {
-            if (zxa_front == true) // If to fill from front
-            {
-                while (zxa_string.Length < zxa_length) // If string is too short
-                {
-                    zxa_string = zxa_filler + zxa_string; // Lengthen string with filler character
-                }
-                return zxa_string; // Return string
-            }
-            else // If to fill from end
-            {
-                while (zxa_string.Length < zxa_length) // If string is too short
-                {
-                    zxa_string = zxa_string + zxa_filler; // Lengthen string with filler character
-                }
-                return zxa_string; // Return string
-            }
+            writer.WriteLine("QSO: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}",
+                 BandHelper.ToMHzString(c.Band).PadRight(4),
+                 ModeHelper.ToCabrilloString(c.Mode),
+                 c.StartTime.ToString("yyyy-MM-dd HHmm"),
+                 c_CallUsed.PadRight(15).ToUpper(),
+                 c_LocatorSent.ToUpper(),
+                 c.ReportSent.PadRight(3),
+                 c.SerialSent.ToString().PadRight(3),
+                 c.Callsign.PadRight(15).ToUpper(),
+                 c.LocatorReceived.ToString().ToUpper(),
+                 c.ReportReceived.PadRight(3),
+                 c.SerialReceived.ToString().PadRight(3));
         }
     }
 }

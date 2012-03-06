244e3b078e32b3056fc7b57fe3248c82a3a3687a
W C <wclark.uk@googlemail.com>
2012-03-06 00:58:38 +0000

    Add prompt for Cabrillo headers

--- Engine/CabrilloExporter.cs
+++ Engine/CabrilloExporter.cs
@@ -7,35 +7,36 @@ namespace Engine
 {
     public static class CabrilloExporter
     {
-        const string c_CallUsed = "G6UW"; // Callsign sent
-        const string c_LocatorSent = "JO02af"; // Locator sent
-        const string c_Contest = "RSGB-UKAC"; // Contest name (optional)
-        const string c_Operators = "G3ZAY M0ZXA"; // Operators
-        const string c_ClaimedScore = ""; // Claimed score (optional)
-
-        public static void ExportContacts(IEnumerable<Contact> contacts, string exportLocation)
+        public static void ExportContacts(IEnumerable<Contact> contacts, string exportLocation, string c_LocatorSent, string c_CallUsed, string c_Operators, string c_Contest, string c_ClaimedScore)
         {
             using (StreamWriter writer = new StreamWriter(exportLocation))
             {
                 writer.WriteLine("START-OF-LOG: 2.0");
                 writer.WriteLine("CREATED-BY: M0VFC CamLog");
-                if (string.IsNullOrEmpty(c_Contest))
+                if (!string.IsNullOrEmpty(c_Contest))
                 {
                     writer.WriteLine("CONTEST: " + c_Contest.ToUpper());
                 }
                 writer.WriteLine("CALLSIGN: " + c_CallUsed.ToUpper());
-                if (string.IsNullOrEmpty(c_ClaimedScore))
+                if (!string.IsNullOrEmpty(c_ClaimedScore))
                 {
                     writer.WriteLine("CLAIMED SCORE: " + c_ClaimedScore);
                 }
-                writer.WriteLine("OPERATORS: " + c_Operators.ToUpper());
+                if (!string.IsNullOrEmpty(c_Operators))
+                {
+                    writer.WriteLine("OPERATORS: " + c_Operators.ToUpper());
+                }
+                else
+                {
+                    writer.WriteLine("OPERATORS: " + c_CallUsed.ToUpper());
+                }
                 writer.WriteLine();
                 foreach (Contact c in contacts)
-                    ExportContact(c, writer);
+                    ExportContact(c, writer, c_LocatorSent, c_CallUsed);
             }
         }
 
-        private static void ExportContact(Contact c, StreamWriter writer)
+        private static void ExportContact(Contact c, StreamWriter writer, string c_LocatorSent, string c_CallUsed)
         {
             writer.WriteLine("QSO: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}",
                  BandHelper.ToMHzString(c.Band).PadRight(4),
@@ -44,11 +45,11 @@ namespace Engine
                  c_CallUsed.PadRight(15).ToUpper(),
                  c_LocatorSent.ToUpper(),
                  c.ReportSent.PadRight(3),
-                 c.SerialSent.ToString().PadRight(3),
+                 c.SerialSent.ToString().PadLeft(3),
                  c.Callsign.PadRight(15).ToUpper(),
                  c.LocatorReceived.ToString().ToUpper(),
                  c.ReportReceived.PadRight(3),
-                 c.SerialReceived.ToString().PadRight(3));
+                 c.SerialReceived.ToString().PadLeft(3));
         }
     }
 }

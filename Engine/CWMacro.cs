8c84632c4aeb76b9ebe060253f6068890a2d7004
Robert Chipperfield <robert@syxis.co.uk>
2012-04-17 00:22:09 +0100

    Start of CW Macro support

---
+++ Engine/CWMacro.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public sealed class CWMacro
+    {
+        private ContactStore m_ContactStore;
+
+        public ContactStore ContactStore
+        {
+            get { return m_ContactStore; }
+            set { m_ContactStore = value; LoadMacros(); }
+        }
+
+        public WinKey WinKey { get; set; }
+
+        private List<string> m_Macros = new List<string>(12);
+
+        public CWMacro(ContactStore store)
+        {
+            ContactStore = store;
+            LoadMacros();
+        }
+
+        public string GetMacro(int number)
+        {
+            if (number < m_Macros.Count)
+                return m_Macros[number];
+            else
+                return null;
+        }
+
+        public void SetMacro(int number, string text)
+        {
+            m_Macros[number] = text;
+        }
+
+        public void SendMacro(int number, Dictionary<string, string> values)
+        {
+            string s = GetMacro(number);
+            if (s == null)
+                return;
+            
+            // Untokenize the macro
+
+            WinKey.SendString(s);
+        }
+
+        private void LoadMacros()
+        {
+            m_Macros = new List<string>
+            {
+                "CQ GS3PYE/P GS3PYE/P K",
+                "M0VFC 5NN",
+                "TU GS3PYE/P"
+            };
+        }
+    }
+}

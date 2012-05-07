301434cb568a6d55136a8cb9b7cc08ff1da3536d
Robert Chipperfield <robert@syxis.co.uk>
2012-05-07 18:06:31 +0100

    Add FixADIFDates for fixing my stupid import of the FP log with YYYY-MM-MM dates

--- EngineTests/AdifTests.cs
+++ EngineTests/AdifTests.cs
@@ -17,5 +17,28 @@ namespace EngineTests
             ContactStore store = new ContactStore("localhost", "fp2011", "root", "aopen");
             contacts.ForEach(store.SaveContact);
         }
+
+        [Test]
+        public void FixAdifDates()
+        {
+            List<Contact> adifContacts = AdifHandler.ImportAdif(@"C:\temp\test.adi");
+            ContactStore store = new ContactStore("localhost", "fp2011", "root", "aopen");
+
+            foreach (Contact adifContact in adifContacts)
+            {
+                // Find the matching one in the DB
+                List<Contact> existingContacts = store.GetPreviousContacts(adifContact.Callsign);
+                List<Contact> probableMatches = existingContacts.FindAll(c => c.EndTime.TimeOfDay == adifContact.EndTime.TimeOfDay && c.Band == adifContact.Band);
+                if (probableMatches.Count == 1)
+                {
+                    probableMatches[0].StartTime = probableMatches[0].EndTime = adifContact.EndTime;
+                    //store.SaveContact(probableMatches[0]);
+                }
+                else
+                {
+                    Assert.Fail("Failed to find single matching QSO: " + adifContact);
+                }
+            }
+        }
     }
 }

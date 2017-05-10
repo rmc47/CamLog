6709f331fbc54efcad836ad8c061f96ff3c27cec
Robert Chipperfield <robert@syxis.co.uk>
2017-05-10 16:08:10 +0100

    Additional modes in mode map

---
+++ Engine/DBMigrations/MySQL/8.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `modemap` (`sourceText`, `displayText`) VALUES ('MSK144', 'JT/FSK');
+INSERT INTO `modemap` (`sourceText`, `displayText`) VALUES ('FSK315', 'JT/FSK');
+
+UPDATE `setup` SET `val`=8 WHERE `key`='schemaVersion';

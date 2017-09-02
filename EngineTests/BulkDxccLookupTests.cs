012ace79aa58eef5de84c88d4e323274c8407bf9
Robert Chipperfield <robert@syxis.co.uk>
2017-09-02 12:52:54 +0100

    Add DXCCByOperator test

--- EngineTests/BulkDxccLookupTests.cs
+++ EngineTests/BulkDxccLookupTests.cs
@@ -25,5 +25,26 @@ namespace EngineTests
             var results = lookup.Lookup(new List<string> { "DE1ABC" });
             Assert.That(results["DE1ABC"] == 1000, "Expected DE1ABC to map to INVALID (SWL)");
         }
+
+        [Test]
+        public void DXCCByOperator()
+        {
+            var cs = new ContactStore("server.dx", "2017_08_yota", "g3pye", "g3pyeflossie");
+            var contacts = cs.GetAllContacts(null);
+            var contactsByOps = contacts.GroupBy(c => c.Operator);
+            Dictionary<string, int> opDxccCounts = new Dictionary<string, int>();
+            foreach (var contactsByOp in contactsByOps)
+            {
+                var op = contactsByOp.Key;
+                var uniqueCallsigns = contactsByOp.Select(c => c.Callsign).Distinct();
+                var dxccs = new BulkDxccLookup().Lookup(uniqueCallsigns);
+                var dxccCount = dxccs.Values.Distinct().Count();
+                opDxccCounts[op] = dxccCount;
+            }
+            foreach (var countEntry in opDxccCounts.OrderByDescending(o => o.Value))
+            {
+                Console.WriteLine("{0}\t\t{1}", countEntry.Key, countEntry.Value);
+            }
+        }
     }
 }
